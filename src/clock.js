21d2495a59d61d814b7f272e886d40dd3919ec86
blingblin-g <luvbliny@gmail.com>
2021-06-15 01:02:01 +0900

    Add 0 below 10

--- src/clock.js
+++ src/clock.js
@@ -6,15 +6,17 @@ let seconds = today.getSeconds();
 console.log(hours + " : " + minutes + " : " + seconds);
 
 let now = "";
-const clock = document.querySelector(".clock");
+const clock = document.querySelector(".js-clock");
 function getTime() {
   today = new Date();
   hours = today.getHours();
   minutes = today.getMinutes();
   seconds = today.getSeconds();
 
-  now = hours + " : " + minutes + " : " + seconds;
+  now = `${hours < 10 ? `0${hours}` : hours} : ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } : ${seconds < 10 ? `0${seconds}` : seconds}`;
   clock.innerHTML = `<div>${now}</div>`;
 }
 
-setInterval(() => getTime(), 1000);
+setInterval(getTime, 1000);
